e4c567ef8b20ba5d9f7943bc631d11fbc52fc24a
Vlad Carstea <vlad.carstea@plentymarkets.com>
2022-11-09 12:42:55 +0200

    allow null weight for partnered SP packages

--- src/AmazonPHP/SellingPartner/Model/FulfillmentInbound/PartneredSmallParcelPackageOutput.php
+++ src/AmazonPHP/SellingPartner/Model/FulfillmentInbound/PartneredSmallParcelPackageOutput.php
@@ -277,7 +277,7 @@ class PartneredSmallParcelPackageOutput implements \ArrayAccess, \JsonSerializab
     /**
      * Gets weight.
      */
-    public function getWeight() : Weight
+    public function getWeight() : ?Weight
     {
         return $this->container['weight'];
     }
